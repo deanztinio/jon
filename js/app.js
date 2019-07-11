e6f51db96d58870693654426c114c5dbcc3b5402
deanztinio <52593580+deanztinio@users.noreply.github.com>
2019-07-11 12:59:19 +0800

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,15 @@
+new hoverEffect({
+    parent:document.querySelector('.distortion1'),
+    intensity: 0.2,
+    image1: './images/jon3.png',
+    image2: './images/jon4.png',
+    displacementImage: './images/heightMap.png'
+});
+
+new hoverEffect({
+    parent:document.querySelector('.distortion2'),
+    intensity: 0.2,
+    image1: './images/jon5.png',
+    image2: './images/jon6.png',
+    displacementImage: './images/heightMap.png'
+});
